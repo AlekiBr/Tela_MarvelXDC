a030f3c6715a0ec559d676c00be1a319ca4ffba2
AlekiBr <cristhiancgabriel02@gmail.com>
2024-12-12 00:36:20 +0000

    up

---
+++ main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($){
+    $(document).on('click', '.next-btn', function(){
+        $(this).toggleClass('active');
+        $('.herosection').toggleClass('exterminador');
+
+        $('h1').text('Exterminador');
+    });
+
+    $(document).on('click', '.next-btn.active', function(){
+
+        $('h1').text('DEEDPOOL');
+    });
+});
