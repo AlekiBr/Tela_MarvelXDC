67f79c865cc92480bf0da29538ce2dc761261d85
AlekiBr <cristhiancgabriel02@gmail.com>
2024-12-12 04:39:54 +0000

    up

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-jQuery(document).ready(function($){
-    $(document).on('click', '.next-btn', function(){
+jQuery(document).ready(function($) {
+    $(document).on('click', '.next-btn', function() {
         $(this).toggleClass('active');
         $('.herosection').toggleClass('exterminador');
 
-        $('h1').text('Exterminador');
+        if ($(this).hasClass('active')) {
+            // Atualizar para Exterminador
+            $('h1').text('Exterminador');
+            $('.description').text(
+                'Exterminador é um estrategista militar brilhante e um assassino mortal. Conhecido por sua habilidade em combate e com armas, ele é um mercenário altamente treinado e formidável. Apesar de seus métodos implacáveis, ele possui um código moral, o que o torna um anti-herói complexo e fascinante.'
+            );
+        } else {
+            // Voltar para Deadpool
+            $('h1').text('DEADPOOL');
+            $('.description').text(
+                'Deadpool é conhecido por ser um combatente habilidoso, mestre no uso de armas de fogo e espadas, e por sua personalidade caótica e imprevisível. Ele atua como mercenário, aceitando missões que variam de salvar o mundo a fazer tarefas absurdas, sempre com um toque cômico. Apesar de seu exterior cínico e violento, Deadpool ocasionalmente demonstra um lado mais humano, protegendo inocentes e lutando por boas causas – embora, claro, sempre do seu jeito único e cheio de piadas.'
+            );
+        }
     });
-    
-    $(document).on('click', '.next-btn.active', function(){
-
-        $('h1').text('DEADPOOL');
-    });  
 });
